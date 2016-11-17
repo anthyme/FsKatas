604afb0c7e795e5c749536e2598a1cbd3b940281
anthyme <anthyme@gmail.com>
2016-11-17 21:45:12 +0100

    add basic bowling kata

---
+++ Dojo/Bowling.fs
@@ -0,0 +1,17 @@
+﻿module Bowling
+
+open Helpers
+
+let computeScore line =
+    let valueOf = function
+        | '-' -> 0
+        | x -> Int.ofChar x
+    line |> Seq.sumBy valueOf 
+
+module Tests =
+    open FsUnit.Xunit
+    open Xunit
+
+    type ``Given a computeScore`` () =
+
+        [<Fact>] let ``compute simple facts`` () = "9-9-9-9-9-9-9-9-9-9-" |> computeScore |> should equal (9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9 + 9)
