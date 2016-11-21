d556ec776d50522e6a4c698c09c64e03479cac6e
anthyme <anthyme@gmail.com>
2016-11-21 22:17:12 +0100

    add strike tests

--- Dojo/Bowling.fs
+++ Dojo/Bowling.fs
@@ -6,12 +6,14 @@ type Shot =
 | Miss
 | Knock of int
 | Spare
+| Strike
 
 type Frame =
 | Start
 | FirstShot of Shot
 | SimpleShots
 | Spared
+| Striked
 
 type State = { Score:int ; Last:Frame }
 
@@ -19,6 +21,7 @@ let computeScore line =
     let shotOf = function
         | '-' -> Miss
         | '/' -> Spare
+        | 'X' -> Strike
         | x -> Knock (Int.ofChar x)
 
     let valueOf = function | Knock x -> x | _ -> 0
@@ -26,6 +29,7 @@ let computeScore line =
     let folder (state:State) shot = 
         let increment, frame =
             match state.Last, shot with
+            | _, Strike -> 10, Striked
             | _, Spare -> 10, Spared
             | Spared, shot -> (valueOf shot, FirstShot shot)
             | FirstShot(shot1), shot2 -> (valueOf shot1 + valueOf shot2, SimpleShots)
@@ -57,4 +61,8 @@ module Tests =
         [<Fact>]
         let ``compute mixed simple, miss and spares`` () = 
             "5/2/5/5/5/5/-45/5/5/5" |> computeScore |> should equal (10+5 + 10+2 + 10+5 + 10+5 + 10+5 + 10+0 + 0+4 + 10+5 + 10+5 + 10+5)
+
+        [<Fact>]
+        let ``compute strikes`` () = 
+            "XXXXXXXXXXXX" |> computeScore |> should equal (10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10 + 10+10+10)
         
