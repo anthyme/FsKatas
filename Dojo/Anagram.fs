04b2cd4fcba6df221e3a18929f5bbaf7545244f0
anthyme <anthyme@gmail.com>
2017-01-26 00:04:01 +0100

    Add words dictionary

--- Dojo/Anagram.fs
+++ Dojo/Anagram.fs
@@ -1,8 +1,12 @@
 ﻿module Anagram
 
+open Helpers
+open System.Net.Http
+
+let wordsUrl = "http://codekata.com/data/wordlist.txt"
+let words = HttpClient().GetAsync(wordsUrl).Result.Content.ReadAsStringAsync().Result |> String.split '\n' |> List.ofSeq
 
 let findAnagram word = ["god"]
-
 
 module Tests = 
     open Xunit
@@ -10,4 +14,4 @@ module Tests =
 
     type ``Given an anagram finder``() = 
         [<Fact>] 
-        let ``foo should give oof``() = findAnagram "dog" |> should contain "god"
+        let ``dog should give god``() = findAnagram "dog" |> should contain "god"
