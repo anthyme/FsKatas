3fadf74087bf05a3cf55b693d42c17419c35717e
anthyme <anthyme@gmail.com>
2017-01-25 23:45:18 +0100

    Add simple anagram finder

---
+++ Dojo/Anagram.fs
@@ -0,0 +1,13 @@
+﻿module Anagram
+
+
+let findAnagram word = ["god"]
+
+
+module Tests = 
+    open Xunit
+    open FsUnit.Xunit
+
+    type ``Given an anagram finder``() = 
+        [<Fact>] 
+        let ``foo should give oof``() = findAnagram "dog" |> should contain "god"
