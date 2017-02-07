cdebbdb2ad1ec7982f87176496a7371054b0fb25
anthyme <anthyme@gmail.com>
2017-02-07 11:37:04 +0100

    Add DojoCs and c# test tools for Pokwer

---
+++ DojoCs/PokerHand/PokerHandTests.cs
@@ -0,0 +1,11 @@
+﻿using NFluent;
+using Xunit;
+
+namespace DojoCs.PokerHand
+{
+    public class PokerTest
+    {
+        [Fact]
+        public void TestTrue() => Check.That(true).Equals(true);
+    }
+}
